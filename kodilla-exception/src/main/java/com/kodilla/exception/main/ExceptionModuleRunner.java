301319c67cd0097067a9e5b397c7c974f7f954fe
Soroczka <olgasoroka94@yahoo.com>
2018-02-18 23:51:47 +0100

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,25 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+//        fileReader.readFile();
+//
+//        FileReaderWithoutHandling fileReader2 = new FileReaderWithoutHandling();
+//        fileReader2.readFile(); // tu trzeba obsłużyć wyjątek, który został rzucony wyżej z klasy FileReaderWithoutHandling przy pomocy THROWS
+
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e){
+            System.out.println("Problem while reading a file");
+        }
+
+
+
     }
 }
