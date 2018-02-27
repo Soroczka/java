ffb9d278453701f0c7c52f97a1d8239133c8d9d5
Soroczka <olgasoroka94@yahoo.com>
2018-02-27 19:10:14 +0100

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperation.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperation.java
@@ -1,25 +1,27 @@
 package com.kodilla.stream.array;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperation {
 
     static double getAvarage(int[] numbers){
 
-        if(numbers.length > 0){
+
 
             IntStream.range(0, numbers.length)
                     .map(n -> numbers[n])
                     .forEach(System.out::println);
 
-            double elementOfArray = IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n])
-                    .average()
-                    .getAsDouble();
+        final OptionalDouble average = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
+        double elementOfArray = average
+                    .orElse(-1);
 
             return elementOfArray;
 
-        } else return 0;
+
 
     }
 
